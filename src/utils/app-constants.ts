3e87e926dd240b4068cd43d21cd1975da587603f
Daniel Shely <danielshely1@gmail.com>
2020-10-10 01:53:46 +0300

    fetch from heroku

--- src/utils/app-constants.ts
+++ src/utils/app-constants.ts
@@ -7,8 +7,8 @@ export const IMG_DIR = `${process.env.PUBLIC_URL}/assets/images/`;
 export const MOCKS_DIR = `${process.env.PUBLIC_URL}/assets/images/`;
 
 // TODO need to add production url well it will e ready
-export const ACTUALI_SERVER_BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000" : "";
+export const ACTUALI_SERVER_BASE_URL_DEV = "http://localhost:4000";
+export const ACTUALI_SERVER_BASE_URL = "https://actuali-back.herokuapp.com";
 
 export const MIN_CATEGORIES_NUM = 3;
 export const NEWS_EACH_FETCH = 12;
